272458cbb77f2f22b837f014e78cd4eb60e4e5bd
Caitlin Henry <caitlinhenry.ss@gmail.com>
2025-09-08 20:47:47 -0400

    extract helper method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,18 +2,22 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
         int i = 1;
         int count = 0;
 
-        while (i < 1000) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        while (i < n) {
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
 
             if (divisibleBy3 || divisibleBy5) {
                 count++;
             }
             i += 1;
         }
-        System.out.println(count);
+        return count;
     }
 }
